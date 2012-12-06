df10d2b9d6dd3addd021ef3f3bcf88a24b45fe37
daneos <daneos@daneos.com>
2012-12-06 11:53:49 +0100

    removed +min and -max

--- analyze.sh
+++ analyze.sh
@@ -70,18 +70,10 @@ minfiles=$(for s in ${counts[@]}; do echo $s; done | sort -n -r - | tail -1)
 
 pmaxb10=$(python -c "print $maxbytes*1.1")
 pmaxb20=$(python -c "print $maxbytes*1.2")
-nmaxb10=$(python -c "print $maxbytes*0.9")
-nmaxb20=$(python -c "print $maxbytes*0.8")
 pmaxf10=$(python -c "print $maxfiles*1.1")
 pmaxf20=$(python -c "print $maxfiles*1.2")
-nmaxf10=$(python -c "print $maxfiles*0.9")
-nmaxf20=$(python -c "print $maxfiles*0.8")
-pminb10=$(python -c "print $minbytes*1.1")
-pminb20=$(python -c "print $minbytes*1.2")
 nminb10=$(python -c "print $minbytes*0.9")
 nminb20=$(python -c "print $minbytes*0.8")
-pminf10=$(python -c "print $minfiles*1.1")
-pminf20=$(python -c "print $minfiles*1.2")
 nminf10=$(python -c "print $minfiles*0.9")
 nminf20=$(python -c "print $minfiles*0.8")
 
@@ -89,24 +81,16 @@ echo "------------------------------------------------------------------"
 echo "MAX:     $(python -c "print $maxbytes/1048576") MB -- ($maxbytes bytes)"
 echo "MAX+10%: $(python -c "print $pmaxb10/1048576") MB -- ($pmaxb10 bytes)"
 echo "MAX+20%: $(python -c "print $pmaxb20/1048576") MB -- ($pmaxb20 bytes)"
-echo "MAX-10%: $(python -c "print $nmaxb10/1048576") MB -- ($nmaxb10 bytes)"
-echo "MAX-20%: $(python -c "print $nmaxb20/1048576") MB -- ($nmaxb20 bytes)"
 echo "------------------------------------------------------------------"
 echo "MAX:     $maxfiles files"
 echo "MAX+10%: $pmaxf10 files"
 echo "MAX+20%: $pmaxf20 files"
-echo "MAX-10%: $nmaxf10 files"
-echo "MAX-20%: $nmaxf20 files"
 echo "------------------------------------------------------------------"
 echo "MIN:     $(python -c "print $minbytes/1048576") MB -- ($minbytes bytes)"
-echo "MIN+10%: $(python -c "print $pminb10/1048576") MB -- ($pminb10 bytes)"
-echo "MIN+20%: $(python -c "print $pminb20/1048576") MB -- ($pminb20 bytes)"
 echo "MIN-10%: $(python -c "print $nminb10/1048576") MB -- ($nminb10 bytes)"
 echo "MIN-20%: $(python -c "print $nminb20/1048576") MB -- ($nminb20 bytes)"
 echo "------------------------------------------------------------------"
 echo "MIN:     $minfiles files"
-echo "MIN+10%: $pminf10 files"
-echo "MIN+20%: $pminf20 files"
 echo "MIN-10%: $nminf10 files"
 echo "MIN-20%: $nminf20 files"
 echo "------------------------------------------------------------------"
